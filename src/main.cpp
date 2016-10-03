e11712bddcde571025998a31737a53843aa5bcf7
Isabelle <isabelle.forsman@gmail.com>
2016-10-03 16:40:18 +0200

    begun L_out calculations

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
 
 #define D_EPSILON 0.001
 
+const unsigned int C_MAX_BOUNCES = 5;
+
 const std::array<Sphere, 1> c_spheres{
 	{Sphere(glm::vec3(8.0f, 0.0f, -1.0f), 1.0f, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0) }
 };
@@ -19,6 +21,8 @@ const std::array<Sphere, 1> c_spheres{
 const glm::vec3 pointLightPos = glm::vec3(5.0f, 3.0f, 0.0f);
 
 inline void renderPixels(Image* img, Camera* cam);
+inline PixelRGB L_in(Ray* ray);
+inline PixelRGB L_out(Ray* ray);
 inline bool findClosestIntersection(Ray* ray, RayIntersectionData* intersectionData);
 inline bool perfectReflectedRay(Ray* ray, RayIntersectionData* intersectionData);
 inline bool shadowRay(RayIntersectionData* intersectionData);
@@ -64,21 +68,72 @@ inline void renderPixels(Image* img, Camera* cam)
 
 		ray.m_dir = glm::normalize(ray.m_pos - cam->m_pos);
 
-		RayIntersectionData iD;
-		iD.m_material.m_diffuse.m_r = 0.0f;
-		iD.m_material.m_diffuse.m_g = 0.0f;
-		iD.m_material.m_diffuse.m_b = 0.0f;
+		PixelRGB pixelColor = L_in(&ray);
 
-		findClosestIntersection(&ray, &iD);
-
-		perfectReflectedRay(&ray, &iD);
-
-		shadowRay(&iD);
-
-		img->imgData.r[i] = iD.m_material.m_diffuse.m_r;
-		img->imgData.g[i] = iD.m_material.m_diffuse.m_g;
-		img->imgData.b[i] = iD.m_material.m_diffuse.m_b;
+		img->imgData.r[i] = pixelColor.m_r;
+		img->imgData.g[i] = pixelColor.m_g;
+		img->imgData.b[i] = pixelColor.m_b;
 	}
+}
+
+inline PixelRGB L_in(Ray* ray)
+{
+	RayIntersectionData iD;
+	iD.m_material.m_diffuse.m_r = 0.0f;
+	iD.m_material.m_diffuse.m_g = 0.0f;
+	iD.m_material.m_diffuse.m_b = 0.0f;
+
+	findClosestIntersection(ray, &iD);
+
+	perfectReflectedRay(ray, &iD);
+
+	shadowRay(&iD);
+
+	return iD.m_material.m_diffuse;
+}
+
+inline PixelRGB L_out(RayIntersectionData* inData, Ray* ray, int nrBounces)
+{
+	PixelRGB rayMissColor;
+	rayMissColor.m_r = 1.0f;
+	rayMissColor.m_g = 0.2f;
+	rayMissColor.m_b = 0.0f;
+
+	if (nrBounces == C_MAX_BOUNCES)
+		return rayMissColor;
+
+	PixelRGB inEmmissive = inData->m_material.m_emmisive;
+	PixelRGB inDiffuse = inData->m_material.m_diffuse;
+
+	PixelRGB result = inEmmissive;
+
+	// randomize ray direction
+	Ray newRay;
+	
+	//TODO write a random ray dir generator for the hemisphere
+	glm::vec3 randRayDir;
+
+	// TODO: possibly move the start ray out a bit from the surface to avoid the 
+	// self intersection problem
+	newRay.m_pos = inData->m_intersectionPoint;
+	newRay.m_dir = glm::normalize(randRayDir);
+
+
+	// calculate the rendering eq
+	RayIntersectionData data;
+	if (findClosestIntersection(&newRay, &data)) {
+		
+		// TODO: the rendering eq
+
+		result.m_r += 0.0f; 
+		result.m_g += 0.0f;
+		result.m_b += 0.0f;
+	}
+	else {
+
+	}
+
+
 }
 
 inline bool findClosestIntersection(Ray* ray, RayIntersectionData* intersectionData)
@@ -102,7 +157,7 @@ inline bool perfectReflectedRay(Ray* ray, RayIntersectionData* intersectionData)
 	glm::vec3 N = intersectionData->m_normal;
 	glm::vec3 I = ray->m_pos + ray->m_dir;
 
-	reflectedRay.m_dir = I - 2 * glm::dot(I, N) * N;
+	reflectedRay.m_dir = I - 2.0f * glm::dot(I, N) * N;
 
 	RayIntersectionData reflectedRayIntersection;
 	reflectedRayIntersection.m_material.m_diffuse.m_r = 0.0f;
@@ -111,9 +166,9 @@ inline bool perfectReflectedRay(Ray* ray, RayIntersectionData* intersectionData)
 
 	findClosestIntersection(&reflectedRay, &reflectedRayIntersection);
 
-	intersectionData->m_material.m_diffuse.m_r = intersectionData->m_material.m_diffuse.m_r * 0.8 + reflectedRayIntersection.m_material.m_diffuse.m_r * 0.2;
-	intersectionData->m_material.m_diffuse.m_g = intersectionData->m_material.m_diffuse.m_g * 0.8 + reflectedRayIntersection.m_material.m_diffuse.m_g * 0.2;
-	intersectionData->m_material.m_diffuse.m_b = intersectionData->m_material.m_diffuse.m_b * 0.8 + reflectedRayIntersection.m_material.m_diffuse.m_b * 0.2;
+	intersectionData->m_material.m_diffuse.m_r = intersectionData->m_material.m_diffuse.m_r * 0.8f + reflectedRayIntersection.m_material.m_diffuse.m_r * 0.2f;
+	intersectionData->m_material.m_diffuse.m_g = intersectionData->m_material.m_diffuse.m_g * 0.8f + reflectedRayIntersection.m_material.m_diffuse.m_g * 0.2f;
+	intersectionData->m_material.m_diffuse.m_b = intersectionData->m_material.m_diffuse.m_b * 0.8f + reflectedRayIntersection.m_material.m_diffuse.m_b * 0.2f;
 
 	return true;
 }
